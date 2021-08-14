383fa683d1d6885dedc876dfad91fc2f46bbc002
takagi <iam.takagi.dev@gmail.com>
2021-08-15 00:35:36 +0900

    Cleanup depends

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
-// @ts-nocheck
-// import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer"
 const path = require("path")
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin")
 const isProduction = process.env.NODE_ENV === "production"
 
 /** @type {import("webpack").Configuration} */
@@ -22,25 +19,16 @@ const config = {
                 test: /\.tsx?$/,
                 loader: "ts-loader",
                 options: { compilerOptions: { module: "esnext", moduleResolution: "node" } },
-            },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            },
+            }
         ],
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".json"],
     },
     devtool: "source-map",
-    plugins: [
-        new MiniCSSExtractPlugin()
-    ],
     devServer: {
         publicPath: "/assets/",
         contentBase: "public",
-    },
-    optimization: {
     },
 }
 
